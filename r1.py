79616ae038d51daeecdf57bd825ee5eed1d646c5
Andrew Filippov <andrew@filippov.by>
2015-08-13 22:32:09 +0300

    Change st

--- r1.py
+++ r1.py
@@ -19,19 +19,20 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+
 import shlex, subprocess
 
-def main(args):
 
-	f=open("np.txt")  # read file like "username@domain password" 
-	for line in f:
-		k=str.split(line)
-		subprocess.check_call(["echo", "I will run :", "imapsync --login1",  k[0], " --password1 ", k[1]])
-	print ("Ok")
-	return 0
+def main(args):
+    f = open("np.txt")  # read file like "username@domain password"
+    for line in f:
+        k = str.split(line)
+        subprocess.check_call(["echo", "I will run :", "imapsync --login1", k[0], " --password1 ", k[1]])
+    print("Ok")
+    return 0
+
 
 if __name__ == '__main__':
     import sys
+
     sys.exit(main(sys.argv))
